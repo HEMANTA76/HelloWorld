dcbc510c81aab119fa830c9eadeb4733c7d3a8f3
sasmita <sasmeeta.swain@gmail.com>
2020-10-26 22:22:19 -0400

    This is the first check in

---
+++ HelloWorldSample/src/com/main/Sample.java
@@ -0,0 +1,13 @@
+package com.main;
+
+public class Sample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is my first java program with Jenkins");
+		for (int i =0;i<=10;i++) {
+			System.out.println("Hello World "+ i);
+		}
+	}
+
+}
